2d2bce3e7dc3e39449fdee3f3306c0afe3179c5a
fsannicolo <federico.sannicolo@marconirovereto.it>
2026-02-19 11:42:29 +0100

    feat: tree structure

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Node a = new Node('A');
+        Node b = new Node('B');
+        Node c = new Node('C');
+        Node d = new Node('D');
+        Node e = new Node('E');
+        Node f = new Node('F');
+        Node g = new Node('G');
+        Node h = new Node('H');
+
+        BinaryTree tree = new BinaryTree(a);
+        a.setLeft(b);
+        a.setRight(c);
+        b.setLeft(d);
+        c.setLeft(e);
+        c.setRight(f);
+        f.setLeft(g);
+        f.setRight(h); 
     }
 }
---
+++ src/BinaryTree.java
@@ -0,0 +1,14 @@
+public class BinaryTree {
+    
+    private Node root;
+
+    public BinaryTree() {
+
+        this.root = null;
+    }
+
+    public BinaryTree(Node n) {
+
+        this.root = n;
+    }
+}
