8b3df69c287bcb0499c5d02de9982d559b3bd3c7
fsannicolo <federico.sannicolo@marconirovereto.it>
2026-02-19 12:08:34 +0100

    fest: depth-first traversals

--- src/App.java
+++ src/App.java
@@ -9,14 +9,26 @@ public class App {
         Node f = new Node('F');
         Node g = new Node('G');
         Node h = new Node('H');
+        Node i = new Node('I');
 
         BinaryTree tree = new BinaryTree(a);
         a.setLeft(b);
         a.setRight(c);
         b.setLeft(d);
-        c.setLeft(e);
+        b.setRight(e);
+        e.setLeft(g);
+        e.setRight(h);
         c.setRight(f);
-        f.setLeft(g);
-        f.setRight(h); 
+        f.setLeft(i); 
+
+        System.out.println("Preorder");
+        tree.preorder();
+        System.out.println(""); 
+        System.out.println("Inorder");
+        tree.inorder();
+        System.out.println(""); 
+        System.out.println("Postorder");
+        tree.postorder();
+        System.out.println(""); 
     }
 }
--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -11,4 +11,53 @@ public class BinaryTree {
 
         this.root = n;
     }
+
+    // depth-first
+    public void preorder() {
+        preorder(root);
+    }
+
+    /**
+     * I nodi genitori sono visitati prima dei nodi figli
+     * @param n nodo di partenza
+     */
+    private void preorder(Node n) {
+
+        // exit clause
+        if (n == null) return;
+
+        System.out.print(n.getData()); 
+        preorder(n.getLeft());
+        preorder(n.getRight()); 
+    }
+
+    public void inorder() {
+        inorder(root);
+    }
+
+    // in-order
+    private void inorder(Node n) {
+
+        // exit clause
+        if (n == null) return;
+
+        inorder(n.getLeft());
+        System.out.print(n.getData()); 
+        inorder(n.getRight()); 
+    }
+
+    public void postorder() {
+        postorder(root);
+    }
+
+    // post-order
+    private void postorder(Node n) {
+
+        // exit clause
+        if (n == null) return;
+
+        postorder(n.getLeft());
+        postorder(n.getRight()); 
+        System.out.print(n.getData()); 
+    }
 }
